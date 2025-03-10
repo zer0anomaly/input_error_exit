83521a7b3a9ff31addcd98f1503e8c125aefe382
zer0anomaly <zeroanomale@gmail.com>
2025-03-10 10:23:20 +0500

    Add files via upload

---
+++ input_exit.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{	
+	int c;
+	printf("Type to any chracter to exit !!!\n");
+	c = getchar();
+	printf("You typed the %c exiting the program !!!", c);
+	return 0;
+}
